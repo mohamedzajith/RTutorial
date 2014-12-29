f2eb41f097a6a5ca0ff7bf4c40ec5977e64a2c89
MohamedZajith <mohamedzajith@gmail.com>
2014-12-29 20:19:49 +0530

    this code basic math function +-/*^ and more

---
+++ basicMathFunction.R
@@ -0,0 +1,31 @@
+#basic math functions
+2+3
+3/2
+4*2
+6-9
+2^3
+
+#square root sqrt()
+sqrt(100)
+
+#absolute value abs()
+abs(3-21)
+
+#trig function(radians) -->sin(),cos(),tan()
+sin(pi/6)
+
+#pi the number 3.14...
+4*pi
+
+#exponential and logaitham
+log(1)
+exp(3)
+
+#euler's gamma function
+gamma(20)
+
+#factorial function
+factorial(6)
+
+#combination functions
+choose(5,2)
